eebe1432f1599b19e2f3e3c0faf0f1934d96d48c
parker-1331 <parker.1331@buckeyemail.osu.edu>
2024-11-05 15:09:46 -0500

    Removed unused import statement.

--- ceilingfanapp/src/components/viewOrder.jsx
+++ ceilingfanapp/src/components/viewOrder.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import {createPortal} from "react-dom";
 import {useLocation, useNavigate} from 'react-router-dom';
 import {Container, Row, Col, Button, Modal} from 'react-bootstrap';
 import { DisplayOrderSummary } from "./confirmation";
